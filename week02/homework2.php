f9f876408bbabf783864b80f86f6cd95a4aefb67
a1113301 <126176971+a1113301@users.noreply.github.com>
2024-06-16 21:48:38 +0800

    Add files via upload

---
+++ week02/homework2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body bgcolor="	#BBFFFF">
+<div class="container">
+        <h1>自我介紹</h1>
+        <div class="profile">
+            <img src="images/b6cdccd6-0760-4be6-bd9b-d26df3592de4.jpg" width="500" height="500">
+            <div class="info">
+            <h3><font size="4" color="#336666">基本資料</font></h3>
+                <ul>
+                    <li><b>姓名</b>:&nbsp;&nbsp;馬寬宇</li>
+                    <li><b>學號</b>:&nbsp;&nbsp;A1113301</li>
+                    <li><b>性別</b>:&nbsp;&nbsp;男</li>
+                </ul>
+                <h2>就讀學校:高雄大學</h2>
+                <a href="https://www.nuk.edu.tw/">
+                    <img src="images/images.jpg" width="700" height="500">
+                </a>
+                <h2>個人資訊</h2>
+                <p>我是一位高雄大學資訊管理學系的學生</p>
+                <p>聯絡方式：<br>
+                    電子郵件：a1113301@mail.nuk.edu.tw<br>
+                    電話：12345678
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
